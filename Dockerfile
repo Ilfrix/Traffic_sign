0055475ed2416d595b1a51b74ce576e2ebcd8a3e
Kerlenton <semyonschv@yandex.ru>
2024-05-16 11:53:24 +0300

    fixing

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-# Используйте официальный образ Python как родительский образ
+# Используем официальный образ Python как родительский образ
 FROM python:3.8
 
-# Установите рабочий каталог в контейнере
+# Установим рабочий каталог в контейнере
 WORKDIR /usr/src/app
 
-# Установите зависимости для OpenCV
+# Установим зависимости для OpenCV
 RUN apt-get update && apt-get install -y libgl1-mesa-glx && rm -rf /var/lib/apt/lists/*
 
-# Скопируйте файлы зависимостей и установите их
+# Скопируем файлы зависимостей и установим их
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Скопируйте все файлы, кроме тех, что в .dockerignore
+# Скопируем все файлы, кроме тех, что в .dockerignore
 COPY . .
 
 # Команда для запуска приложения
--- gui.py
+++ gui.py
@@ -1,15 +1,15 @@
+# Импорт необходимых библиотек
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
 from PIL import ImageTk, Image
-
 import numpy
-# load the trained model to classify sign
 from keras.models import load_model
 
+# Загрузка предварительно обученной модели для классификации знаков
 model = load_model('my_model.h5')
 
-# dictionary to label all traffic signs class.
+# Словарь для маркировки всех классов дорожных знаков.
 classes = {1: 'Speed limit (20km/h)',
            2: 'Speed limit (30km/h)',
            3: 'Speed limit (50km/h)',
@@ -54,16 +54,17 @@ classes = {1: 'Speed limit (20km/h)',
            42: 'End of no passing',
            43: 'End no passing veh > 3.5 tons'}
 
-# initialise GUI
+# Инициализация графического интерфейса пользователя
 top = tk.Tk()
 top.geometry('800x600')
 top.title('Traffic sign classification')
 top.configure(background='#CDCDCD')
 
+# Создание метки для отображения результата классификации
 label = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
 sign_image = Label(top)
 
-
+# Функция для классификации изображения
 def classify(file_path):
     global label_packed
     image = Image.open(file_path)
@@ -76,13 +77,13 @@ def classify(file_path):
     print(sign)
     label.configure(foreground='#011638', text=sign)
 
-
+# Функция для отображения кнопки "Classify Image"
 def show_classify_button(file_path):
     classify_b = Button(top, text="Classify Image", command=lambda: classify(file_path), padx=10, pady=5)
     classify_b.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
     classify_b.place(relx=0.79, rely=0.46)
 
-
+# Функция для загрузки изображения
 def upload_image():
     try:
         file_path = filedialog.askopenfilename()
@@ -97,14 +98,17 @@ def upload_image():
     except:
         pass
 
-
+# Создание и размещение кнопки загрузки изображения
 upload = Button(top, text="Upload an image", command=upload_image, padx=10, pady=5)
 upload.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
-
 upload.pack(side=BOTTOM, pady=50)
+
+# Размещение элементов интерфейса
 sign_image.pack(side=BOTTOM, expand=True)
 label.pack(side=BOTTOM, expand=True)
 heading = Label(top, text="Know Your Traffic Sign", pady=20, font=('arial', 20, 'bold'))
 heading.configure(background='#CDCDCD', foreground='#364156')
 heading.pack()
+
+# Запуск основного цикла Tkinter
 top.mainloop()
